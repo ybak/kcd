ee6e7af34801b081d0bb8374061062bae8953841
tuguoqiang <806191630@qq.com>
2019-01-25 14:34:29 +0800

    “19-01-25 整合后的工行贷

--- src/com/model1/ManagementCenter/kj_icbc.java
+++ src/com/model1/ManagementCenter/kj_icbc.java
@@ -2,6 +2,8 @@ package com.model1.ManagementCenter;
 
 import com.model.icbc.icbc;
 
+
+
 public class kj_icbc extends icbc{
 
 }
--- src/com/model1/ManagementCenter/kjs_icbc_cardk.java
+++
@@ -1,7 +0,0 @@
-package com.model1.ManagementCenter;
-
-import com.model.icbc.icbc_cardk;
-
-public class kjs_icbc_cardk extends icbc_cardk{
-
-}
