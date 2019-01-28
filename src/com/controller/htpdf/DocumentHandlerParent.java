266856fc43a21c1dddde0b4498bb1f3fdd25dea3
LiWang <2649416836@qq.com>
2019-01-28 16:24:57 +0800

    修改

--- src/com/controller/htpdf/DocumentHandlerParent.java
+++ src/com/controller/htpdf/DocumentHandlerParent.java
@@ -39,8 +39,8 @@ public abstract class DocumentHandlerParent{
 	protected File file = null;
 	
 	//根目录 这里的是全局的 包括图片和.pdf都在这个目录的下级
-	//public static String root_Directory="DIMG/assess/";
-	public static String root_Directory="C:/Users/Administrator/Desktop/word/haha1/";
+	public static String root_Directory="DIMG/assess/";
+	//public static String root_Directory="C:/Users/Administrator/Desktop/word/haha1/";
 	
 	//拼接中间目录
 	private String savepdfpath="upload/"+new SimpleDateFormat("yyyy/MM/dd/").format(new Date());
