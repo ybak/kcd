3b01a4b252d505b36b05dce41885ec833384d966
tuguoqiang <806191630@qq.com>
2019-03-07 15:34:05 +0800

    更新完善

---
+++ src/com/service1/icbc_banklist/icbc_banklistService.java
@@ -0,0 +1,11 @@
+package com.service1.icbc_banklist;
+
+import java.util.List;
+
+import com.model1.icbc.erp.PageData;
+
+public interface icbc_banklistService {
+
+	// 获取icbc_banklist 所有信息
+	List<PageData> geticbc_banklist();
+}
