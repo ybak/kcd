8fa23287b782ab5a197dec8165e477a1791c3396
708823141 <708823141@qq.com>
2019-04-01 16:32:58 +0800

    update limit 0,1

--- src/com/controller/erp_icbc/loanAfter/LoanAutomaticTaskController.java
+++ src/com/controller/erp_icbc/loanAfter/LoanAutomaticTaskController.java
@@ -34,8 +34,8 @@ public class LoanAutomaticTaskController {
 	@Autowired
 	private LoanOverdueService loanOverdueService;
 	 
-	@Scheduled(cron="0/5 * *  * * ? ")   //每5秒执行一次   
-//	@Scheduled(cron = "0 0 1 * * ?") //每天凌晨1点执行
+//	@Scheduled(cron="0/5 * *  * * ? ")   //每5秒执行一次   
+	@Scheduled(cron = "0 0 1 * * ?") //每天凌晨1点执行
 	public void tasktest(){	
 		// 1. 准备连接数据库的 4 个字符串.
         // 驱动的全类名.
@@ -75,8 +75,8 @@ public class LoanAutomaticTaskController {
 	
 	
 	//从逾期修改到电催
-	@Scheduled(cron="0/6 * *  * * ? ")   //每6秒执行一次   
-//	@Scheduled(cron = "0 0 6 * * ?") //每天凌晨6点执行
+//	@Scheduled(cron="0/6 * *  * * ? ")   //每6秒执行一次   
+	@Scheduled(cron = "0 0 6 * * ?") //每天凌晨6点执行
 	public void tasktestTo(){	
 		// 1. 准备连接数据库的 4 个字符串.
         // 驱动的全类名.
